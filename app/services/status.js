dbf3b174680da41fbf96a4fe97a77d82b3252743
Benoit Rajalu <rajalubenoit@gmail.com>
2016-08-11 22:18:00 +0200

    updated environment sutff

--- app/services/status.js
+++ app/services/status.js
@@ -10,16 +10,16 @@ export default Ember.Service.extend({
 
   about() {
     this.setProperties({'toWork': false, 'toAbout': true});
-    console.log('status: about')
+    console.log('status: about');
   },
 
   work() {
     this.setProperties({'toWork': true, 'toAbout': false});
-    console.log('status: work')
+    console.log('status: work');
   },
 
   empty() {
     this.setProperties({'toWork': false, 'toAbout': false});
-    console.log('status: empty')
+    console.log('status: empty');
   }
 });
--- app/transitions.js
+++ app/transitions.js
@@ -19,4 +19,4 @@ export default function(){
     this.use('toRight'),
     this.reverse('toLeft')
   );
-};
+}
