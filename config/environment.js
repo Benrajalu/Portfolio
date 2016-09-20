456d635413e8fb212862f8ee85b615d7467e2e5d
Benoit Rajalu <rajalubenoit@gmail.com>
2016-09-20 09:08:39 +0200

    Changed URL type to HTML5 push state (because IE<10 is dead to me) and updated header contents

--- config/environment.js
+++ config/environment.js
@@ -48,7 +48,7 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
     ENV.rootURL = '/';
-    ENV.locationType = 'hash';
+    ENV.locationType = 'history';
   }
 
   return ENV;
